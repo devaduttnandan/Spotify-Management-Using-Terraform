ba2dcbe481c6e9e9fe904efa2f5d802ff3a2cf76
Devadutt Nandan <113972660+devaduttnandan@users.noreply.github.com>
2025-02-06 08:25:12 +0530

    Update playlist.tf

--- spotify management using terraform/playlist.tf
+++ spotify management using terraform/playlist.tf
@@ -1,10 +1,24 @@
-data "spotify_search_track" "kendrick"{
-    artist = "Kendrick Lamar"
+data "spotify_search_track" "Juice_Wrld" {
+  artist = "Juice Wrld"
+}
+resource "spotify_playlist" "playlist" {
+  name = "Juice G.O.A.T"
+
+  tracks = [
+    data.spotify_search_track.Juice_Wrld.tracks[0].id,
+    data.spotify_search_track.Juice_Wrld.tracks[1].id,
+    data.spotify_search_track.Juice_Wrld.tracks[2].id,
+  ]
 }
 
-resource "spotify_playlist" "kendrick_lamar"{
-    name = "kendrick G.O.A.T"
-    tracks = [data.spotify_search_track.kendrick.tracks[0].id,
-    data.spotify_search_track.kendrick.tracks[1].id,
-    data.spotify_search_track.kendrick.tracks[2].id]
+data "spotify_search_track" "imported_playlist" {
+  artist = "The Weeknd"
+  limit = 10
 }
+
+
+resource "spotify_playlist" "trial" {
+  name   = "hiphop+++"
+  tracks = [for track in data.spotify_search_track.imported_playlist.tracks : track.id]
+}
+
